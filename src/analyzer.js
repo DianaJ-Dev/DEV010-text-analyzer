65ff17a0af50af2fcef7164ec02686e8a01d1c73
Diana <juliethdiana123@gmail.com>
2023-07-24 17:25:47 -0500

    primera version analyzer

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,49 @@
 const analyzer = {  
-  getWordCount: (text) => {
+  getWordCount: (text) => { 
+    return text.split(/\s+/).length;
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
+  
   getCharacterCount: (text) => {
+    if (text.trim() === '') {
+      return 0;
+    }
+    return text.length;
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
+
   getCharacterCountExcludingSpaces: (text) => {
+    return text.replace(/[^\w]/g, "").length;
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (text) => {    
+   
+  getAverageWordLength: (text) => {  
+    const words = text.split(' ');
+    let totalLength = 0;
+    for (let i= 0; i <words.length; i++) {
+      totalLength += words[i].length;
+    }
+    const  averageLength = totalLength / words.length;
+    return parseFloat(averageLength.toFixed(2));
+
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const regex = /\b\d+(\.\d+)?\b/g;
+    const matches = text.match(regex);
+    if (!matches) {
+      return 0;
+    }
+    return matches.length;
   },
   getNumberSum: (text) => {
+    const numbers = text.split(' ').filter(word => !isNaN(word)); 
+    const sum = numbers.reduce((acc, curr) => acc + parseFloat(curr),0); 
+    return sum;
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
   },
 };
+
+
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,63 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+const editor = document.querySelector('[name="user-input"]');
+const wordCount = document.querySelector('.wordCount');
+editor.addEventListener('keyup', function() {
+  const text = editor.value;
+  const count = analyzer.getWordCount(text);
+  wordCount.innerHTML = 'Total palabras '+ count; 
+});
+
+const retornarcaracteres = document.querySelector('[name="user-input"]');
+const charactercount = document.querySelector('.characterCount');
+retornarcaracteres.addEventListener('keyup', function() {
+  const text = retornarcaracteres.value;
+  const count = analyzer.getCharacterCount(text)
+  charactercount.innerHTML = 'Recuento caracteres '+ count;
+});
+
+const recuentosiespacios = document.querySelector('[name="user-input"]');
+const characternospacescount = document.querySelector('.characterNoSpacesCount');
+recuentosiespacios.addEventListener('keyup', function() {
+  const text = recuentosiespacios.value;
+  const count = analyzer.getCharacterCountExcludingSpaces(text)
+  characternospacescount.innerHTML = 'Recuento total caracteres '+ count;
+});
+
+const mediadepalabras = document.querySelector('[name="user-input"]');
+const wordlengthaverage = document.querySelector('.wordLengthAverage');
+mediadepalabras.addEventListener('keyup',function(){
+  const text = mediadepalabras.value;
+  const count = analyzer.getAverageWordLength(text);
+  wordlengthaverage.innerHTML = 'longitud media palabras '+ count;
+});
+
+const Recuentonumeros = document.querySelector('[name="user-input"]');
+const numbercount = document.querySelector('.numberCount');
+Recuentonumeros.addEventListener('keyup', function(){
+  const text = Recuentonumeros.value;
+  const count = analyzer.getNumberCount(text);
+  numbercount.innerHTML = 'Total numeros '+ count ;
+  
+})
+
+const Totalnumeros = document.querySelector('[name="user-input"]');
+const datatestid = document.querySelector('.dataTestId');
+Totalnumeros.addEventListener('keyup',function(){
+  const text= Totalnumeros.value;
+  const count = analyzer.getNumberSum(text);
+  datatestid.innerHTML= 'Suma todos los número '+ count;
+})
+
+const miTextArea = document.querySelector('[name="user-input"]');
+const resetButton = document.getElementById('reset-button'); 
+resetButton.addEventListener('click',function() {
+  miTextArea.value='';
+  wordCount.innerHTML="0";
+  charactercount.innerHTML="0";
+  characternospacescount.innerHTML="0";
+  numbercount.innerHTML="0";
+  datatestid.innerHTML="0";
+  wordlengthaverage.innerHTML="0";
+});
